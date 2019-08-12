83acd5216fcfe86cfd6fc7e54b346ee31179c891
Tim Allen <tim@noblesamurai.com>
2019-08-13 09:13:44 +1000

    semistandard

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-module.exports = function(db, credentials, initialRefreshToken) {
-const knex = require('knex')(db),
-      SimpleOAuth2 = require('simple-oauth2'),
-      oauth2 = SimpleOAuth2.create(credentials),
-      debug = require('debug')('oauth2-refresh-token');
+module.exports = function (db, credentials, initialRefreshToken) {
+  const knex = require('knex')(db);
+  const SimpleOAuth2 = require('simple-oauth2');
+  const oauth2 = SimpleOAuth2.create(credentials);
+  const debug = require('debug')('oauth2-refresh-token');
+  const path = require('path');
 
   var migrated;
-  function migrate() {
+  function migrate () {
     if (migrated) return migrated;
-    migrated = knex.migrate.latest({ directory: __dirname + '/migrations' });
+    migrated = knex.migrate.latest({ directory: path.join(__dirname, '/migrations') });
     return migrated;
   }
 
-  function latestTokens() {
-    return migrate().then(function() {
+  function latestTokens () {
+    return migrate().then(function () {
       return knex('tokens').orderBy('created_at', 'desc').first('refresh_token', 'access_token');
     });
   }
 
-  function refresh() {
-    return migrate().then(function() {
+  function refresh () {
+    return migrate().then(function () {
       return latestTokens();
-    }).then(function(tokens) {
-      refreshToken = tokens ? tokens.refresh_token : initialRefreshToken;
+    }).then(function (tokens) {
+      const refreshToken = tokens ? tokens.refresh_token : initialRefreshToken;
       var token = oauth2.accessToken.create({ refresh_token: refreshToken });
 
-      return token.refresh().then(function(result) {
+      return token.refresh().then(function (result) {
         const rec = ({ access_token: result.token.access_token, refresh_token: result.token.refresh_token });
         debug('inserting new tokens', rec);
         return knex('tokens').insert(rec);
@@ -34,8 +35,8 @@ const knex = require('knex')(db),
 
   return {
     refresh: refresh,
-    getAccessToken: function() {
-      return latestTokens().then(function(tokens) {
+    getAccessToken: function () {
+      return latestTokens().then(function (tokens) {
         if (tokens) return tokens.access_token;
       });
     },
